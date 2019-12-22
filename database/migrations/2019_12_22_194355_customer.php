131e455585a0d64dc0545924432a22cec27e3f94
unknown <henry.olushola@techvibesltd.com>
2019-12-22 20:48:22 +0100

    core(database/migrations/2019_12_22_194355_customer.php):confihured  customer migrations

---
+++ database/migrations/2019_12_22_194355_customer.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Customer extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customer', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('user_id');
+            $table->string('payment_type');
+            $table->string('address');
+            $table->string('area');
+            $table->string('state');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customer');
+    }
+}
