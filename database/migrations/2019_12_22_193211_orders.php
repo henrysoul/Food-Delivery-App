6c72f5703b01cee6159e312f14a765e0716659a0
unknown <henry.olushola@techvibesltd.com>
2019-12-22 20:43:13 +0100

    core(database/migrations/2019_12_22_193211_orders.php) :configured orders migration

---
+++ database/migrations/2019_12_22_193211_orders.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Orders extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('customer_id');
+            $table->bigInteger('menu_id');
+            $table->smallInteger('price');
+            $table->smallInteger('quantity');
+            $table->string('description');
+            $table->string('address');
+            $table->string('area');
+            $table->string('state');
+            $table->string('payment_type');
+            $table->boolean('delivery_status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
