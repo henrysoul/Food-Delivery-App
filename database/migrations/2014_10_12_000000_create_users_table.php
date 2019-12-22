de9d12b156e048683c7edf06009fbfc2579d39fe
unknown <henry.olushola@techvibesltd.com>
2019-12-22 20:19:56 +0100

    core(user_groups):created user_groups migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->bigIncrements('group_id');
+            $table->bigInteger('group_id');
             $table->string('name');
             $table->string('email')->unique();
             $table->bigInteger('phone');
