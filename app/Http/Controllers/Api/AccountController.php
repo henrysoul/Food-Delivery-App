0c258570cd59be811969dd4cbee46acc3356e0cb
unknown <henry.olushola@techvibesltd.com>
2019-12-22 22:58:00 +0100

    feat(app/Http/Controllers/Api/AccountController.php):created login & register Api)

--- app/Http/Controllers/Api/AccountController.php
+++ app/Http/Controllers/Api/AccountController.php
@@ -4,22 +4,65 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Validator;
+use Hash;
+use App\User;
+
 class AccountController extends Controller
 {
+    // this function creates a customer
     public function register(Request $request){
+        // vaildates http request
         $validator = Validator::make($request->all(),[
             'first_name'=>'required',
             'last_name'=>'required',
             'email'=>'required|email|unique:users',
-            'phone'=>'phone|required',
-            'password'=>'required',
-            'password_confirm'=>'requred|same:password'
+            'phone'=>'required|max:11',
+            'password'=>'required|min:6',
+            'password_confirm'=>'required|same:password|min:6'
         ]);
 
         if($validator->fails()){
-            return response()->json(['status_code'=>500,'message'=>'validation error']);
+            // report error
+            return response()->json(['status'=>400,'message'=>'Bad Request','error'=>$validator->errors()]);
+        }else{
+            try {
+                // creates user
+                $user = User::create(['group_id'=>100,'name'=>$request->first_name.' '.$request->last_name,'email'=>$request->email,
+                    'phone'=>$request->phone,'password'=>Hash::make($request->password),
+                ]);
+                $success = ['token'=>$user->createToken('MyApp')->accessToken,'name'=>$user->name];
+
+                return response()->json(['status'=>200,'message'=>'ok','success'=>$success]);
+            } catch (\Exception $ex) {
+                // return error in creating user
+                return response()->json(['status'=>500,'message'=>'Internal Server Error']);
+            }
         }
-        return response()->json(['status_code'=>200,$request->all()]);
+
+    }
+
+    public function login(Request $request){
+        // vaildates http request
+        $validator = Validator::make($request->all(),[
+            'email'=>'required|email',
+            'password'=>'required',
+        ]);
+
+        if($validator->fails()){
+            // report error
+            return response()->json(['status'=>400,'message'=>'Bad Request','error'=>$validator->errors()]);
+        }else{
+                // checks users credentials
+                if(Auth::attempt(['email'=>$request->email,'password'=>$request->password])){
+                    $user = Auth()->user();
+                    $success = ['token'=>$user->createToken('MyApp')->accessToken,'name'=>Auth()->user()->name,'email'=>Auth()->user()->email];    
+                    return response()->json(['status'=>200,'message'=>'ok','success'=>$success]);
+                }else{
+                    // return error in creating user
+                    return response()->json(['error'=>'Unauthorised'],400);
+                }
+        }
     }
 }
