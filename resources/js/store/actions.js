92f1cf6e17e9b9d99a72827dded98d135be6ab7e
Olushola Henry <henry.olushola@techvibesltd.com>
2019-12-25 20:50:34 +0100

    core(store):configured redux

---
+++ resources/js/store/actions.js
@@ -0,0 +1,3 @@
+export const LOGIN_USER = 'LOGIN_USER';
+export const ERROR_ALERT = 'ERROR_ALERT';
+export const DISMISS_ALERT = 'DISMISS_ALERT';
---
+++ resources/js/store/reducer/auth.js
@@ -0,0 +1,34 @@
+import * as actionTypes from '../actions';
+
+const initialState = {
+    isAuthenticated: false,
+    token : null,
+    alert: false,
+    error: false,
+    alertMessage: null
+}
+const reducer = (state = initialState,action) => {
+    switch ( action.type ) {
+        case actionTypes.LOGIN_USER:
+            return {
+                ...state,
+                isAuthenticated : !state.isAuthenticated,
+                token: action.userObj
+            }
+        case actionTypes.ERROR_ALERT:
+            return {
+                ...state,
+                alert : true,
+                error: true,
+                alertMessage: action.message
+            } 
+        case actionTypes.DISMISS_ALERT:
+            return {
+                ...state,
+                alert : !state.alert
+            }
+
+    }
+    return state;
+}
+export default reducer;
