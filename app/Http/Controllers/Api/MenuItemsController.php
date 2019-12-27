19fe7283e5c3c0cfa153f2742b83f95402a7fd8f
Olushola Henry <henryolusholasolomon@gmail.com>
2019-12-27 12:22:14 +0100

    feat(MenuItemsController):added edit menuitem and delete menuitem features

--- app/Http/Controllers/Api/MenuItemsController.php
+++ app/Http/Controllers/Api/MenuItemsController.php
@@ -9,6 +9,16 @@ use Validator;
 
 class MenuItemsController extends Controller
 {
+    public function  index(){
+        try {
+            
+            $items = MenuItems::orderBy('food_type')->get();
+            return response()->json(['data'=>$items],200); 
+
+        } catch (\Exception $ex) {
+            return response()->json(['message'=>'Internal Server Error'],500);
+        }
+    }
     public function save(Request $request){
         // vaildates http request
         $validator = Validator::make($request->all(),[
@@ -42,4 +52,34 @@ class MenuItemsController extends Controller
                }
         }
     }
+
+    public function update(Request $request,$id){
+        try {
+            if($request->hasfile('picture')){
+                $file = $request->file('picture');
+                $photo_url = "photo".uniqid().'.'.$file->getClientOriginalExtension();
+                $file->move(public_path("images"),$photo_url);
+                $request['picture'] = $photo_url;
+                MenuItems::find($id)->update(['food_type'=>$request->food_type,'price'=>$request->price,'available'=>$request->available,
+                    'quantity'=>$request->quantity,'picture'=>$photo_url,'description'=>$request->description
+                ]); 
+            }else{
+                MenuItems::find($id)->update($request->all());
+            }
+            return response()->json(['message'=>"Item updated successfully"],200); 
+        } catch (\Exception $ex) {
+            return response()->json(['message'=>'Internal Server Error'],500);
+        }
+    }
+
+    public function delete($id){
+        try {
+            MenuItems::find($id)->delete();
+            return response()->json(['message'=>"Item deleted successfully"],200); 
+        } catch (\Exception $ex) {
+            return response()->json(['message'=>'Internal Server Error'],500);
+        }
+    }
+
+    
 }
