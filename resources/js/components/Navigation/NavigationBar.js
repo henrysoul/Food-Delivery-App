b649703a86058d9514cbc7528c1d84cd90043ee7
Olushola Henry <henryolusholasolomon@gmail.com>
2019-12-27 12:17:01 +0100

    fix(NavigationBar.js):added logout action

--- resources/js/components/Navigation/NavigationBar.js
+++ resources/js/components/Navigation/NavigationBar.js
@@ -16,6 +16,32 @@ const Styles = styled.div`
     }
 `;
 
+const logoutHandler=()=>{
+    localStorage.removeItem('jwtToken');
+    localStorage.removeItem('bearer');
+    localStorage.removeItem('email');
+    localStorage.removeItem('name');
+    localStorage.removeItem('group_id');
+    localStorage.removeItem('token');
+    localStorage.removeItem('Token');
+    localStorage.removeItem('phone');
+    window.location.replace('/');
+}
+
+let items = (
+        <React.Fragment>
+            <Nav.Item><Nav.Link href="login">Login</Nav.Link></Nav.Item>
+            <Nav.Item><Nav.Link href="signup">Signup</Nav.Link></Nav.Item>
+        </React.Fragment>  
+    );
+    if(localStorage.bearer){
+        items = (
+            <React.Fragment>
+                <Nav.Item><Nav.Link onClick={logoutHandler}>Logout</Nav.Link></Nav.Item>
+                <Nav.Item><Nav.Link href="logout">Checkout</Nav.Link></Nav.Item>
+            </React.Fragment>
+        )
+    }
 export  const NavigationBar = () =>(
     <Styles>
         <Navbar expand="lg">
@@ -25,8 +51,7 @@ export  const NavigationBar = () =>(
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ml-auto">
                         <Nav.Item><Nav.Link href="/">Home</Nav.Link></Nav.Item>
-                        <Nav.Item><Nav.Link href="login">Login</Nav.Link></Nav.Item>
-                        <Nav.Item><Nav.Link href="signup">Signup</Nav.Link></Nav.Item>
+                        {items}
                     </Nav>
                 </Navbar.Collapse>
             </Container>  
